f6810c2927d16202072e4c3db5a3e499e24ab710
solidity <solidity>
2022-04-03 19:43:17 +0800

    chores

--- 7-start-bsc.sh
+++ 7-start-bsc.sh
@@ -3,5 +3,7 @@ cp ~/bsc-fast-deploy/config/*  /data/bsc-deploy/
 cp ~/bsc-fast-deploy/config/bsc.service /usr/lib/systemd/system/
 systemctl daemon-reload
 
+chmod +x /data/bsc-deploy/chaind.sh
+
 service bsc start
 tail -f /data/bsc-deploy/logs/bsc.log
