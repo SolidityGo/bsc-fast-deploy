7364e911a843b5c0f47baf15204ac342f1f8f6bb
solidity <solidity>
2022-04-04 04:52:59 +0800

    chores

--- 4-1-bsc-fastnode.sh
+++ 4-1-bsc-fastnode.sh
@@ -7,7 +7,7 @@ cp ./build/bin/geth /usr/local/bin/bsc
 
 mkdir -p /data/bsc-deploy
 cp ~/bsc-fast-deploy/fastnode-config/*  /data/bsc-deploy/
-cp ~/bsc-fast-deploy/fastnode-config/bsc.service /usr/lib/systemd/system/
+cp ~/bsc-fast-deploy/fastnode-config/bsc.service /etc/systemd/system/
 systemctl daemon-reload
 
 chmod +x /data/bsc-deploy/chaind.sh
