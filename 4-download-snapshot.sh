4b5d61b99bb25dfecc8800314556cad87ca84f6a
solidity <solidity>
2022-04-04 00:33:21 +0800

    chores

--- 4-download-snapshot.sh
+++ 4-download-snapshot.sh
@@ -1,4 +1,4 @@
-cd /data
+cd /data &&
 
 nohup aria2c -o geth.tar.lz4 -x 4 -s 12 \
 https://tf-dex-prod-public-snapshot-site1.s3-accelerate.amazonaws.com/geth-20220401.tar.lz4?AWSAccessKeyId=AKIAYINE6SBQPUZDDRRO&Signature=Sd4bS%2Ff%2BY6LzrV00Y%2BrhihCDgqs%3D&Expires=1651482430 \
