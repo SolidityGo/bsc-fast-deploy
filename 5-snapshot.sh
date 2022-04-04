fa235dba9eefabe46d71c5d0dd7f54d9a18910c2
solidity <solidity>
2022-04-05 05:48:38 +0800

    feat: update new snapshot

--- 5-snapshot.sh
+++ 5-snapshot.sh
@@ -9,7 +9,7 @@ aria2c -o geth.tar.lz4 -x 4 -s 12 \
 https://tf-dex-prod-public-snapshot-site1.s3-accelerate.amazonaws.com/geth-20220403.tar.lz4?AWSAccessKeyId=AKIAYINE6SBQPUZDDRRO&Signature=aXdK0jPtdRihyj1vaX%2Ffwq340%2BE%3D&Expires=1651655212 \
 https://tf-dex-prod-public-snapshot.s3-accelerate.amazonaws.com/geth-20220403.tar.lz4?AWSAccessKeyId=AKIAYINE6SBQPUZDDRRO&Signature=AXwfbVPYr4gDFUXcdAoFJBE3Ad4%3D&Expires=1651655213 \
 https://tf-dex-prod-public-snapshot-site3.s3-accelerate.amazonaws.com/geth-20220403.tar.lz4?AWSAccessKeyId=AKIAYINE6SBQPUZDDRRO&Signature=U2y2CtRjbkdX4b9%2BmTYRFNd0U3I%3D&Expires=1651655213 \
-&&  tar -I lz4 -xvf ./geth.tar.lz4
+&&  tar -I lz4 -xvf ./geth.tar.lz4 && echo "---------lz4 Finished---------"
 
 # init
 echo "init node"
